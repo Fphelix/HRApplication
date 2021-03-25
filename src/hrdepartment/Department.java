ea3fbd3076555f93590d3538b68cd0b4062efd57
Fphelix <77512995+Fphelix@users.noreply.github.com>
2021-03-25 00:45:40 -0400

    Add files via upload

---
+++ src/hrdepartment/Department.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrdepartment;
+
+/**
+ *
+ * @author wwstudent
+ */
+public class Department {
+    
+    private int[]Employee;
+    
+    public int [] addEmployee()
+    {
+        
+    }
+    
+    public double totalSalary ()
+    {
+       
+    }
+    public double avgSalary()
+    {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
